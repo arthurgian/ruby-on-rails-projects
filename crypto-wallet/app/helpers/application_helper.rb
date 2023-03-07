2378e92c20ac70584634b1e0b9d3b43c03764bea
Arthur Gian <89954777+arthurgian@users.noreply.github.com>
2023-03-07 20:02:50 -0300

    Environment helper insertion

--- crypto-wallet/app/helpers/application_helper.rb
+++ crypto-wallet/app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def brl_date (us_date)
     us_date.strftime("%d/%m/%y")
   end
+
+  def rails_environment
+    if Rails.env.development?
+    "Desenvolvimento"
+    elsif Rails.env.production?
+    "Produção"
+    else
+    "Teste"
+    end
+  end
 end
